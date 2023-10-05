b21bb4c62675d25ce5f6ecca078245357643facc
9244388815 <144380442+9244388815@users.noreply.github.com>
2023-10-05 16:18:32 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,36 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+    else:
+      print("Invalid deposit amount. Please enter a positive value.")
+
+  def withdraw(self, amount):
+    if 0 < amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+    else:
+      print("Insufficient funds or invalid withdrawal amount.")
+
+  def display_balance(self):
+    print(
+        f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
+    )
 
 
-number = int(input("Enter a value: "))
-res = fact_rec(number)
+# Example usage:
+if __name__ == "__main__":
+  # Create an instance of BankAccount
+  my_account = BankAccount("123456789", "John Doe", 1000.0)
 
-print("The factorial of {} is {}.".format(number, res))
+  # Deposit and withdraw money
+  my_account.deposit(500)
+  my_account.withdraw(200)
+  my_account.display_balance()
--- main (2).py
+++ main (2).py
@@ -1,9 +1,29 @@
-input_year = int(input("Enter the Year to be checked: "))
-if (input_year % 400 == 0):
-  print("%d is a Leap Year" % input_year)
-elif (input_year % 100 == 0):
-  print("%d is Not the Leap Year" % input_year)
-elif (input_year % 4 == 0):
-  print("%d is a Leap Year" % input_year)
-else:
-  print("%d is Not the Leap Year" % input_year)
+class student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  return sorted_students
+
+
+students = [
+    student("durga", "A123", 7.8),
+    student("kalai", "A124", 8.9),
+    student("radhika", "A125", 9.1),
+    student("abi", "A126", 9.9),
+]
+sorted_students = sort_students(students)
+
+#Print the sorted list ofstudent
+
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
